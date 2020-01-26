aae0e81baa0a16543f67595ef9fdd5c7b5ae0a30
djones007 <59663927+djones007@users.noreply.github.com>
2020-01-26 21:37:58 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 # Set files as executable
 sudo chmod +x set-python-ver.sh
+sudo chmod +x raspi-update.sh
 
-Set default Python version to 3
+# Set default Python version to 3
 ./set-python-ver.sh
+
+# Run update script
+./raspi-update.sh
